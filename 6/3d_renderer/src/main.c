57a885efbe81fa17c56408bb0bc7be4a4ab0211b
Paulo <Costa>
2023-05-30 15:09:59 -0400

    .

--- 6/3d_renderer/src/main.c
+++ 6/3d_renderer/src/main.c
@@ -84,14 +84,13 @@ void update(void) {
 }
 
 void draw_grid(void) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            if (x % 10 == 0 || y % 10 == 0) {
-                color_buffer[(window_width * y) + x] = 0xFF444444;
-            }
+    for (int y = 0; y < window_height; y += 10) {
+        for (int x = 0; x < window_width; x += 10) {
+            color_buffer[(window_width * y) + x] = 0xFFFFFFFF;
         }
     }
 }
+
 
 void render_color_buffer(void) {
     SDL_UpdateTexture(
