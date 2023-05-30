579c121146b66bd74ae27028d0e2def15f29f275
Paulo <Costa>
2023-05-30 14:51:13 -0400

    .

--- 5/3d_renderer/src/main.c
+++ 5/3d_renderer/src/main.c
@@ -84,7 +84,16 @@ void update(void) {
 }
 
 void draw_grid(void) {
-    
+    for(int y = 0 ; y < window_height; y++)
+    {
+        for(int x = 0 ; x < window_width; x++)
+        {
+            if(x%10 == 0 || y % 10 == 0)
+            {
+                color_buffer[(window_width*y) + x] = 0xFF444444;
+            }
+        }
+    }
 }
 
 void render_color_buffer(void) {
