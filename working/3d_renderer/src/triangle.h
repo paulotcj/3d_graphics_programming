208cd1489a0bcb907c9e226d78a4f9150a6e7d63
Paulo <Costa>
2023-07-07 22:11:04 -0400

    .

--- working/3d_renderer/src/triangle.h
+++ working/3d_renderer/src/triangle.h
@@ -8,13 +8,13 @@ typedef struct {
     int a;
     int b;
     int c;
-	uint32_t color;
+    uint32_t color;
 } face_t;
 
 typedef struct {
     vec2_t points[3];
-	uint32_t color;
-	float avg_depth;
+    uint32_t color;
+    float avg_depth;
 } triangle_t;
 
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
