2c128766816a57b2eac9200fa5c268c5c039115a
Paulo <Costa>
2023-07-07 21:51:40 -0400

    .

--- working/3d_renderer/src/display.h
+++ working/3d_renderer/src/display.h
@@ -5,8 +5,20 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-#define FPS 60 
+#define FPS 60
 #define FRAME_TARGET_TIME (1000 / FPS)
+
+enum cull_method {
+    CULL_NONE,
+    CULL_BACKFACE
+} cull_method;
+
+enum render_method {
+    RENDER_WIRE,
+    RENDER_WIRE_VERTEX,
+    RENDER_FILL_TRIANGLE,
+    RENDER_FILL_TRIANGLE_WIRE
+} render_method;
 
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
@@ -15,13 +27,12 @@ extern SDL_Texture* color_buffer_texture;
 extern int window_width;
 extern int window_height;
 
-bool initialize_window(void); 
+bool initialize_window(void);
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
-void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
-void render_color_buffer(void); 
+void render_color_buffer(void);
 void clear_color_buffer(uint32_t color);
 void destroy_window(void);
 
--- working/3d_renderer/src/triangle.c
+++ working/3d_renderer/src/triangle.c
@@ -1,7 +1,7 @@
 #include "display.h"
 #include "triangle.h"
 
-void int_swap(int* a, int* b){
+void int_swap(int* a, int* b) {
     int tmp = *a;
     *a = *b;
     *b = tmp;
@@ -20,21 +20,23 @@ void int_swap(int* a, int* b){
 //  (x1,y1)------(x2,y2)
 //
 ///////////////////////////////////////////////////////////////////////////////
-void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color){
-
+void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     // Find the two slopes (two triangle legs)
-	float inv_slope_1 = (float)(x1 - x0) / (y1 - y0);
-	float inv_slope_2 = (float)(x2 - x0) / (y2 - y0);
+    float inv_slope_1 = (float)(x1 - x0) / (y1 - y0);
+    float inv_slope_2 = (float)(x2 - x0) / (y2 - y0);
+
     // Start x_start and x_end from the top vertex (x0,y0)
-	float x_start = x0;
-	float x_end = x0;
+    float x_start = x0;
+    float x_end = x0;
+
     // Loop all the scanlines from top to bottom
-	for(int y = y0; y <= y2; y++ ){
-		draw_line(x_start, y , x_end, y, color);
-		x_start += inv_slope_1;
-		x_end += inv_slope_2;
-	}
+    for (int y = y0; y <= y2; y++) {
+        draw_line(x_start, y, x_end, y, color);
+        x_start += inv_slope_1;
+        x_end += inv_slope_2;
+    }
 }
+
 ///////////////////////////////////////////////////////////////////////////////
 // Draw a filled a triangle with a flat top
 ///////////////////////////////////////////////////////////////////////////////
@@ -50,17 +52,19 @@ void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, u
 ///////////////////////////////////////////////////////////////////////////////
 void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     // Find the two slopes (two triangle legs)
-	float inv_slope_1 = (float)(x2 - x0) / (y2 - y0);
-	float inv_slope_2 = (float)(x2- x1) / (y2 - y1);
+    float inv_slope_1 = (float)(x2 - x0) / (y2 - y0);
+    float inv_slope_2 = (float)(x2 - x1) / (y2 - y1);
+
     // Start x_start and x_end from the bottom vertex (x2,y2)
-	float x_start = x2;
-	float x_end = x2;
+    float x_start = x2;
+    float x_end = x2;
+
     // Loop all the scanlines from bottom to top
-	for(int y = y2; y >= y0; y--) {
-		draw_line(x_start, y, x_end, y, color);
-		x_start -= inv_slope_1;
-		x_end -= inv_slope_2;
-	}
+    for (int y = y2; y >= y0; y--) {
+        draw_line(x_start, y, x_end, y, color);
+        x_start -= inv_slope_1;
+        x_end -= inv_slope_2;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -88,34 +92,43 @@ void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint
 ///////////////////////////////////////////////////////////////////////////////
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     // We need to sort the vertices by y-coordinate ascending (y0 < y1 < y2)
-	if(y0 > y1) {
-		int_swap(&y0, &y1);
-		int_swap(&x0, &x1);
-	} 
-	if(y1 > y2) {
-		int_swap(&y1, &y2);
-		int_swap(&x1, &x2);
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
 	} //because we swapped y1 above we need to compare it again
     if (y0 > y1) {
         int_swap(&y0, &y1);
         int_swap(&x0, &x1);
     }
 
-	if(y1 == y2) {
+    if (y1 == y2) {
         // Draw flat-bottom triangle
-		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
-	} else if(y0 == y1) {
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else if (y0 == y1) {
         // Draw flat-top triangle
-		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
-	} else {
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else {
         // Calculate the new vertex (Mx,My) using triangle similarity
-		int My = y1;
-		int Mx = ( ( (x2 - x0) * (y1-y0) ) / (y2 - y0) )  + x0;
-		
+        int My = y1;
+        int Mx = (((x2 - x0) * (y1 - y0)) / (y2 - y0)) + x0;
+
         // Draw flat-bottom triangle
-		fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
-		
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
         // Draw flat-top triangle
-		fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,color);
-	}
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Draw a triangle using three raw line calls
+///////////////////////////////////////////////////////////////////////////////
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
 }
